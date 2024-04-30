d6799b015b91eae37433dabbf8cc299cffa974f3
Vitaliy <vitaliy@B08-OMK-2096443.local>
2024-04-30 22:22:06 +0300

    make it better1

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 5000
-app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.use(express.json({extended: true}))
 
