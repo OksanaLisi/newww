4450e7b87bca2e9979680ab22e10d2105f8b3cef
Vitaliy <vitaliy@B08-OMK-2096443.local>
2024-04-30 22:02:41 +0300

    deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 require("dotenv").config()
 const cors = require('cors')
+
 const app = express()
 const PORT = process.env.PORT || 5000
 app.use(cors())
