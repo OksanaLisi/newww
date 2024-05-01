67a625284fd59616a3d7f357593935da2a921616
Vitaliy <vitaliy@B08-OMK-2096443.local>
2024-05-01 14:01:03 +0300

    make it better2

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model, Types} = require('mongoose')
 
 const schema = new Schema ({
-    email: {type: String, required: true, unique: true},
+    email: {type: String, required: true},
     password: {type: String, required: true},
     todos: [{type: Types.ObjectId, ref: 'Todo'}]
 })
